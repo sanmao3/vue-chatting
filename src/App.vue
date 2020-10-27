b01fd3dcdea174b0ba64392e90e01e348fd3c981
Sihua Lee <lisihua0916@gmail.com>
2020-10-27 16:38:14 +0800

    feat: 小试vue3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <ChatBox msg="Welcome to Your Vue.js App"/>
+  <ChatBox />
 </template>
 
 <script>
@@ -19,8 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
